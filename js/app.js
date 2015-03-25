fcf33dcdb4c819f643ba766863a4008bbd72d780
Lawrence Tran <lawrencetran1@gmail.com>
2015-03-24 22:14:28 -0700

    added ability to switch back and forth from player1 and player 2

--- js/app.js
+++ js/app.js
@@ -1,32 +1,36 @@
 (function(){
 
 // Declare global variables
-var square = document.getElementsByClassName('square');
-var active = {
-	x: true,
-	o: null
-}
+var squares = document.getElementsByClassName('square');
 
-// Track whose turn it is
-
+// Player 1 = 'O' and Player 2 = 'X'
+var active = {player1: true, player2: false};
 
 // Add add event lisenter to each square
-for (var i = 0; i < square.length; i++) {
-	square[i].addEventListener('click', function() {
-		// this.appendChild(document.createTextNode('x'))
+for (var i = 0; i < squares.length; i++) {
+	squares[i].addEventListener('click', function() {
+
+// When square is clicked, if value of square is true, then exit loop
+	if (this.value) {return}	
+
+// If Player 1 is active, insert 'O', set active.player1 = false and active.player2 = true
+		if (active.player1) {
+				this.appendChild(document.createTextNode('O'));
+				this.value = 'O';
+				active.player1 = false;
+				active.player2 = true;
+		}
+
+// If Player 2 is active, insert 'X', set active.player2 = false and active.player1 = true
+		else if (active.player2) {
+				this.appendChild(document.createTextNode('X'));
+				this.value = 'X';
+				active.player2 = false;
+				active.player1 = true;
+		}
+
 	})
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
