672c31f35df6101114a0dcd20a7bf4f8bc4094cb
Lawrence Tran <lawrencetran1@gmail.com>
2015-03-29 15:51:21 -0700

    added logic to convert to string all rows and columns

--- js/gridController.js
+++ js/gridController.js
@@ -63,7 +63,7 @@
 			self.roundZero = false;
 
 			// initalize elements array to insert move in proper index
-			self.elements = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]; 
+			self.elements = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']; 
 		
 			self.grid = [								// initialize grid to insert corresponding elements into their corresponding position
 							[null,null,null,null,null],
@@ -122,6 +122,7 @@
 
 			self.click = function($index) {
 				// console.log('p1 turn: ' + self.tracker.p1turn, ' p2 turn: ' + self.tracker.p2turn);
+
 				var square = self.squares[$index];	// create square variable using $index to select it
 
 				if (square.used) return;  // check if the square is used, if true...exit function
@@ -145,28 +146,139 @@
 				self.counter++;  		// increment counter on each click, if counter == 9, then it's a tie game
 
 				tracker();				// call tracker function on each click to update grid on each click
-				getWinner();			// call getWinner function on each click to check for winner on each click
+				// getWinner();			// call getWinner function on each click to check for winner on each click
 
 			}
 
 			// Create function that shows current state of the grid when click() is called
 			function tracker() {
 
-				var elementsCopy = [];							// initialize local empty array each time function is called
-				self.elements.forEach(function(ele) {			// for each element of the self.elements array
-					elementsCopy.push(ele);						// push the element into elementsCopy
+				var elementsCopy = [];										// initialize local empty array each time function is called
+				self.elements.forEach(function(ele) {						// for each element of the self.elements array
+					elementsCopy.push(ele);									// push the element into elementsCopy
 				});
-				var newGrid = [];								// initialize local empty array that will act as the updated grid
-				while (elementsCopy.length) {					// loop will run as long as the elementsCopy array has a length of greater than zero
-					newGrid.push(elementsCopy.splice(0,5));		// removes five elements at a time from elementsCopy and pushes it into newGrid
-				};												// this will create an updated 5x5 grid each time tracker function is called
-				console.log(newGrid);
+				var newGrid = [];											// initialize local empty array that will act as the updated grid
+
+				var combos = [];											// initialize empty array to house all the different combinations
+
+				for (var i = 0; i < self.select.length; i++) {				// loop through all grid sizes (3 total)
+					if (self.select[i].active === true) {					// make sure to only select active grid
+						if (self.select[i].id === '3x3 Grid') {				// identify grid id: '3x3 Grid' 
+							while(elementsCopy.length - 16) {				// Acts as a counter for 3x3 Grid, intial value is 9
+								newGrid.push(elementsCopy.splice(0,3));	// remove 3 elements at a time which decreases count at same rate
+							}												// and pushes 3 elements at a time into newGrid, while loop exits when count hits zero
+						}
+						else if (self.select[i].id === '4x4 Grid') {		// identify grid id: '4x4 Grid'
+							while(elementsCopy.length - 9) {				// Acts as a counter for 4x4 Grid, intial value is 16
+								newGrid.push(elementsCopy.splice(0,4));	// remove 4 elements at a time which decreases count at same rate
+							}												// and pushes 4 elements at a time into newGrid, while loop exits when count hits zero
+						}
+						else if (self.select[i].id === '5x5 Grid') {		// identify grid id: '5x5 Grid'
+							while(elementsCopy.length) {					// acts as a counter for 5x5 Grid, initial value is 25
+								newGrid.push(elementsCopy.splice(0,5));	// and pushes 5 elements at a time into newGrid, while loop exits when count hits zero
+							}
+						}						
+					}
+
+				};
+
+				// Push all rows into combos array -  >>>>>>>>>>>  WORKING!!! DO NOT DELETE THIS CODE BLOCK
+				for (var i = 0; i < newGrid.length; i++) {
+					for (var j = 0; j < newGrid[i].length; j++) {
+						if (i == 0 && j == 0) {
+							combos.push(newGrid[i].join(''));
+						}
+						else if (i == 1 && j == 0) {
+							combos.push(newGrid[i].join(''));
+						}
+						else if (i == 2 && j == 0) {
+							combos.push(newGrid[i].join(''));
+						}
+						else if (i == 3 && j == 0) {
+							combos.push(newGrid[i].join(''));
+						}
+						else if (i == 4 && j == 0) {
+							combos.push(newGrid[i].join(''));
+						}
+					}
+				};
+
+				// Push all columns into combos array. Need to rotate newGrid array and use same for loop above to get combos for columns
+				var rotatedNewGrid = invertArray(newGrid, newGrid.length);
+				for (var i = 0; i < rotatedNewGrid.length; i++) {
+					for (var j = 0; j < rotatedNewGrid[i].length; j++) {
+						if (i == 0 && j == 0) {
+							combos.push(rotatedNewGrid[i].join(''));
+						}
+						else if (i == 1 && j == 0) {
+							combos.push(rotatedNewGrid[i].join(''));
+						}
+						else if (i == 2 && j == 0) {
+							combos.push(rotatedNewGrid[i].join(''));
+						}
+						else if (i == 3 && j == 0) {
+							combos.push(rotatedNewGrid[i].join(''));
+						}
+						else if (i == 4 && j == 0) {
+							combos.push(rotatedNewGrid[i].join(''));
+						}
+					}
+				};
+
+				console.log(combos);	
+
 
 				// calculate winning combinations
+				// var row1 = newGrid[0][0] + newGrid[0][1] + newGrid[0][2] + newGrid[0][3] + newGrid[0][4];
+				// var row2 = newGrid[1][0] + newGrid[1][1] + newGrid[1][2] + newGrid[1][3] + newGrid[1][4];
+				// var row3 = newGrid[2][0] + newGrid[2][1] + newGrid[2][2] + newGrid[2][3] + newGrid[2][4];
+				// var row4 = newGrid[3][0] + newGrid[3][1] + newGrid[3][2] + newGrid[3][3] + newGrid[3][4];
+				// var row5 = newGrid[4][0] + newGrid[4][1] + newGrid[4][2] + newGrid[4][3] + newGrid[4][4];
+				// var col1 = newGrid[0][0] + newGrid[1][0] + newGrid[2][0] + newGrid[3][0] + newGrid[4][0];
+				// var col2 = newGrid[0][1] + newGrid[1][1] + newGrid[2][1] + newGrid[3][1] + newGrid[4][1];
+				// var col3 = newGrid[0][2] + newGrid[1][2] + newGrid[2][2] + newGrid[3][2] + newGrid[4][2];
+				// var col4 = newGrid[0][3] + newGrid[1][3] + newGrid[2][3] + newGrid[3][3] + newGrid[4][3];
+				// var col5 = newGrid[0][4] + newGrid[1][4] + newGrid[2][4] + newGrid[3][4] + newGrid[4][4];
+				// var diag1 = newGrid[2][0] + newGrid[3][1] + newGrid[4][2];
+				// var diag2 = newGrid[1][0] + newGrid[2][1] + newGrid[3][2] + newGrid[4][3];
+				// var diag3 = newGrid[0][0] + newGrid[1][1] + newGrid[2][2] + newGrid[3][3] + newGrid[4][4];
+				// var diag4 = newGrid[0][1] + newGrid[1][2] + newGrid[2][3] + newGrid[3][4];
+				// var diag5 = newGrid[0][2] + newGrid[1][3] + newGrid[2][4];
+				// var diag6 = newGrid[2][0] + newGrid[1][1] + newGrid[0][2];
+				// var diag7 = newGrid[3][0] + newGrid[2][1] + newGrid[1][2] + newGrid[0][3];
+				// var diag8 = newGrid[4][0] + newGrid[3][1] + newGrid[2][2] + newGrid[1][3] + newGrid[0][4];
+				// var diag9 = newGrid[4][1] + newGrid[3][2] + newGrid[2][3] + newGrid[1][4];
+				// var diag10 = newGrid[4][2] + newGrid[3][3] + newGrid[2][4];
+
+				// var combos = [row1, row2, row3, row4, row5, col1, col2, col3, col4, col5, diag1, diag2, diag3, diag4, diag5, diag6, diag7, diag8, diag9, diag10];
+
+				// console.log(row1);
+
+				// for (var i = 0; i < combos.length; i++) {
+				// 	var combo = combos[i].split('');
+				// 	for (var j = 0; j < combo.length; j++) {
+				// 		if (combo[j] + combo[j+1] + combo[j+2] === 'XXX') {
+				// 			console.log('X WINS');
+				// 		}
+				// 	}
+				// }
+
+				// for (var i = 0; i < combos.length; i++) {
+				// 	var XXX = combos[i].split('').filter(filterX).join('');
+				// 	var OOO = combos[i].split('').filter(filterO).join('');
+				// 	console.log(XXX, OOO);
+				// 	if (XXX === 'XXX') {
+				// 		console.log('X WINS');
+				// 		self.tracker.winner = 1;
+				// 	}
+				// 	else if (OOO === 'OOO') {
+				// 		console.log('O WINS');
+				// 		self.tracker.winner = 2;
+				// 	}
+				// }
 
 
-
-				// Convert array into 3x3 matrix
+				// // Convert array into 3x3 matrix
 				self.grid[0][0] = self.elements[0];  // top row
 				self.grid[0][1] = self.elements[1];  // top row
 				self.grid[0][2] = self.elements[2];  // top row
@@ -177,7 +289,7 @@
 				self.grid[2][1] = self.elements[7];  // bottom row
 				self.grid[2][2] = self.elements[8];  // bottom row
 
-				// Parse result of grid into a string
+				// // Parse result of grid into a string
 				var topRow 	       = self.grid[0].join('');
 				var middleRow      = self.grid[1].join('');
 				var bottomRow      = self.grid[2].join('');
@@ -187,7 +299,7 @@
 				var diagTopLeft    = self.grid[0][0] + self.grid[1][1] + self.grid[2][2];
 				var diagTopRight   = self.grid[0][2] + self.grid[1][1] + self.grid[2][0];
 
-				// Insert each result into an array
+				// // Insert each result into an array
 				var combinations = [topRow, middleRow, bottomRow, leftCol, middleCol, rightCol, diagTopLeft, diagTopRight];
 
 				// Loop through combinations to check if O or X won and set winMsg accordingly
@@ -200,6 +312,19 @@
 					}
 				};				
 			
+			};
+
+			function invertArray(array, arrayLength) {
+				var newArray = [];
+				for (var i = 0; i < array.length; i++) {
+					newArray.push([]);
+				};
+				for (var i = 0; i < array.length; i++) {
+					for (var j = 0; j < array.length; j++) {
+						newArray[j].push(array[i][j]);
+					};
+				};
+				return newArray;
 			};
 
 			// Create function to check winner. If there is a winner, reset the grid
@@ -217,10 +342,10 @@
 					$timeout(clearGrid, 1000);			// clear grid after 1 second
 				}
 
-				else if (self.tracker.winner === null && self.counter == 9) {		// in there is no winner and counter is 9...
-					self.tracker.tie = true;										// set tie to true to display Tie Game in div with ng-class
-					$timeout(clearGrid, 1000);										// clear grid after 1 second
-				}
+				// else if (self.tracker.winner === null && self.counter == 9) {		// in there is no winner and counter is 9...
+				// 	self.tracker.tie = true;										// set tie to true to display Tie Game in div with ng-class
+				// 	$timeout(clearGrid, 1000);										// clear grid after 1 second
+				// }
 
 			};
 
@@ -236,7 +361,7 @@
 					square.used = false;
 				});
 				self.counter = 0;
-				self.elements = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+				self.elements = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']; 
 				self.grid = [
 							[null,null,null,null,null],
 							[null,null,null,null,null],
